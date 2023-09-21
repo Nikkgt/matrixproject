7bd4125c6251dc6d31359af0c9c76feceae61987
Nikkgt <seglog15200@gmail.com>
2023-09-21 14:14:10 +0300

    matrix_operations was created

---
+++ matrix_operations/src/main/java/org/example/Main.java
@@ -0,0 +1,28 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        int[][] matrix = {{1,6,7,2},
+                          {6,4,3,1},
+                          {1,3,4,1}};
+        int[][] resultMatrix = turnMatrixToTheLeft(matrix);
+        for (int i = 0; i < resultMatrix.length; i++) {
+            for (int j = 0; j < resultMatrix[0].length; j++) {
+                System.out.print(resultMatrix[i][j]+" ");
+            }
+            System.out.println();
+        }
+
+
+    }
+    public static int [][] turnMatrixToTheLeft(int[][] inputMatrix){
+        int [][] turnedMatrix = new int[inputMatrix[0].length][inputMatrix.length];
+
+        for(int j = inputMatrix[0].length-1, k = 0; j>=0; j--, k++){
+            for (int i = 0; i < inputMatrix.length; i++) {
+                turnedMatrix[k][i] = inputMatrix[i][j];
+            }
+        }
+        return turnedMatrix;
+    }
+}
