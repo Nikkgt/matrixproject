a04360a4268102fdf295acab66c1b2ad2d821e2b
Nikkgt <seglog15200@gmail.com>
2023-09-21 17:57:05 +0300

    new method turnToTheRigth added

--- matrix_operations/src/main/java/org/example/Main.java
+++ matrix_operations/src/main/java/org/example/Main.java
@@ -5,7 +5,7 @@ public class Main {
         int[][] matrix = {{1,6,7,2},
                           {6,4,3,1},
                           {1,3,4,1}};
-        int[][] resultMatrix = turnMatrixToTheLeft(matrix);
+        int[][] resultMatrix = turnMatrixToTheRight(matrix);
         for (int i = 0; i < resultMatrix.length; i++) {
             for (int j = 0; j < resultMatrix[0].length; j++) {
                 System.out.print(resultMatrix[i][j]+" ");
@@ -21,6 +21,16 @@ public class Main {
         for(int j = inputMatrix[0].length-1, k = 0; j>=0; j--, k++){
             for (int i = 0; i < inputMatrix.length; i++) {
                 turnedMatrix[k][i] = inputMatrix[i][j];
+            }
+        }
+        return turnedMatrix;
+    }
+    public static int [][] turnMatrixToTheRight(int[][] inputMatrix){
+        int [][] turnedMatrix = new int[inputMatrix[0].length][inputMatrix.length];
+
+        for (int j = 0; j < inputMatrix[0].length; j++) {
+            for (int i = inputMatrix.length-1, k = 0; i >= 0; i--, k++) {
+                turnedMatrix[j][k] = inputMatrix[i][j];
             }
         }
         return turnedMatrix;
